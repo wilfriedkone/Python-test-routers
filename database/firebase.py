5083a0f4919b7c9cb4af3038b88ba8388d856444
wilfried KONE <83975659+wilfriedkone@users.noreply.github.com>
2024-05-10 13:41:45 +0200

    Update firebase.py

--- database/firebase.py
+++ database/firebase.py
@@ -4,15 +4,27 @@ from configs.firebaseconfig import firebaseConfig
 
 from dotenv import dotenv_values
 import json
-
-env = dotenv_values(dotenv_path='.env')
+import os
+ 
+from dotenv import load_dotenv
+ 
+load_dotenv()
+ 
+env={
+    "FIREBASE_SERVICE_ACCOUNT_KEY": os.getenv("FIREBASE_SERVICE_ACCOUNT_KEY"),
+    "FIREBASE_CONFIG": os.getenv("FIREBASE_CONFIG"),
+    "STRIPE_CONFIG": os.getenv("STRIPE_CONFIG")
+}
+ 
+#from configs.firebase_config import firebase_config
+ 
 
 # Initialize Firebase
 if not firebase_admin._apps:
-    cred = firebase_admin.credentials.Certificate("configs/configkey.json")
+    cred = firebase_admin.credentials.Certificate(json.loads(config['FIREBASE_SERVICE_ACCOUNT_KEY']))
     firebase_admin.initialize_app(cred)
 
-firebase = pyrebase.initialize_app(firebaseConfig)
+firebase = pyrebase.initialize_app(json.loads(config['FIREBASE_CONFIG']))
 db = firebase.database()
 
 # Authentication
